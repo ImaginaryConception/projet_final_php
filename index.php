512bdc305efb3bd4c534ea7ee436fcb2afb5b460
Anis Hamouche <anishamouche@gmail.com>
2022-04-19 11:48:33 +0200

    Ajout navbar

--- index.php
+++ index.php
@@ -5,10 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - Wikifruit</title>
-    <?php require 'include/inclusion.php'; ?>
+    <?php require_once 'include/inclusion-css.php'; ?>
 </head>
 <body>
-    <?php include 'include/navbar.php'; ?>
+    <?php require_once 'include/navbar.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="mt-5">
+
+                <h1>Accueil Wikifruit</h1>
+
+            </div>
+
+            <div class="mt-4">
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni repellendus hic soluta ab at ea delectus consectetur, quos suscipit, necessitatibus, impedit blanditiis! Modi fugiat accusantium repellendus impedit repellat molestias a, dolore ab praesentium recusandae quae debitis facere nesciunt eaque quasi alias iste. Explicabo ad deleniti esse aliquam est? Nam, et.</p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php require_once 'include/inclusion-js.php'; ?>
 
 </body>
 </html>
