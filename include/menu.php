f99b3e552258c1f8da33fedf4c95044a94cbec77
Anis Hamouche <anishamouche@gmail.com>
2022-04-19 10:59:04 +0200

    Ajout page d'accueil

---
+++ include/menu.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+<link rel="stylesheet" href="CSS/styles.css">
